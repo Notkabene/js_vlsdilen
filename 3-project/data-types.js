a418cbd8f240d11567bd3717a0aabeb1d3bb308d
Notkabene <len1618@yandex.ru>
2022-01-12 19:35:47 +0300

    Третье задание

---
+++ 3-project/data-types.js
@@ -0,0 +1,10 @@
+const numberType = '42'
+const stringType = 'Строка'
+const booleanType = true
+const nullType = null
+const undefinedType = undefined
+const bigintType = 100n
+const symbolType = Symbol('symbol')
+const objectType = {
+  key: value
+}
---
+++ 3-project/index.js
@@ -0,0 +1,9 @@
+let nameUser = 'Lena'
+let ageUser = 38
+let isLoveJavascript = true
+
+ageUser = 'Вечно молодая'
+isLoveJavascript = null
+
+alert(ageUser)
+alert(isLoveJavascript)
