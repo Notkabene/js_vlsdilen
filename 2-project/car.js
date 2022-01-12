e3864fb6373267d206b55cf6f9a7fa3b189eafe0
Notkabene <len1618@yandex.ru>
2022-01-12 18:47:58 +0300

    Вторая домашка

---
+++ 2-project/car.js
@@ -0,0 +1,25 @@
+const carName = 'Tesla'
+let maxCarSpeed = '320 км/ч'
+let carOwner = 'Илон Маск'
+
+alert(carName)
+alert(maxCarSpeed)
+alert(carOwner)
+
+maxCarSpeed = '350 км/ч'
+carOwner = 'Тони Старк'
+
+console.log('У машины изменилась скорость', maxCarSpeed)
+console.log('У машины изменился владелец', carOwner)
+
+maxCarSpeed = '450 км/ч'
+carOwner = 'Кларк Кент'
+
+console.log('У машины изменилась скорость', maxCarSpeed)
+console.log('У машины изменился владелец', carOwner)
+
+maxCarSpeed = '850 км/ч'
+carOwner = 'Барри Аллен'
+
+console.log('У машины изменилась скорость', maxCarSpeed)
+console.log('У машины изменился владелец', carOwner)
---
+++ 2-project/index.js
@@ -0,0 +1,20 @@
+// JavaScript код с созданием переменных
+
+/*
+Автор:
+Лена Ерофеева
+*/
+
+
+const myName = 'Лена'
+let mySurname = 'Ерофеева'
+let myFavoriteDrink = 'Кофе!'
+const myFavoriteAnimal = 'Котики :)'
+let myFavoriteProgrammingLanguage = 'Ну конечно же JavaScript ;)'
+
+
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
